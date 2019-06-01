bbeb03e57542f296c44f8070d1234d6ee5b67002
Eric Schmucker <eschmucker@unstoppablesoftware.com>
2019-06-01 17:50:52 -0600

    Finished blog project.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,32 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(userId => dispatch(fetchUser(userId)))
+    .value();
+
+  //const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  //userIds.forEach(id => dispatch(fetchUser(id)));
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
-
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 }
+
+export const fetchUser = userId => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${userId}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+}
+
+//export const fetchUser = userId => dispatch => _fetchUser(userId, dispatch);
+
+//const _fetchUser = _.memoize(async (userId, dispatch) => {
+//  const response = await jsonPlaceholder.get(`/users/${userId}`);
+//  dispatch({ type: 'FETCH_USER', payload: response.data });
+//});
